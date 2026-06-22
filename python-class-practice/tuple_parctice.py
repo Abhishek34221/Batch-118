b9952f085b23558abdf2e208f90f82e494416b07
Abhishek34221 <0129abhishek@gmail.com>
2026-06-22 15:17:14 +0530

    first commit

---
+++ python-class-practice/tuple_parctice.py
@@ -0,0 +1,44 @@
+# ------------Tuple------------
+# Definition and properties of tuple.
+# Creation of tuple.
+# indexing and slicing
+# Traversing
+# In-built method
+# tuple comprehension
+# Assignment and class 
+
+
+
+
+# indexing and slicing
+# marks_tuple = (50,55,69,34,89)
+# print(marks_tuple[-1])
+# print(marks_tuple[::-1])
+
+# mutability
+# marks_tuple = (50,55,69,34,89)
+# marks_tuple[2]=500
+# print(marks_tuple)
+
+# Traversing
+# 1.Waf to extract all number greater than 55 , marks_tuple = (50,55,69,34,89)
+# def tuple_fun(m):
+#     new_tuple=[]
+#     for i in m:
+#         if i >=55:
+#             new_tuple.append(i)
+#     return new_tuple
+# marks_tuple = (50,55,69,34,89)
+# res = tuple_fun(marks_tuple)
+# print(res)
+
+
+# 1.Waf to sum of indices of tuple
+# def sum_indice(m):
+#     total = 0
+#     for i in range(len(m)):
+#         total += i
+#     return total
+# marks_tuple = (50, 55, 69, 34, 89)
+# print(sum_indice(marks_tuple))
+
